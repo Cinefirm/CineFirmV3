09e8bd400c22d0ecf1f7400de61b35f55d1b833d
syed abdul muqeeth <syed_abdul_muqeeth@proton.me>
2024-11-02 01:51:49 +0530

    added testimonial

---
+++ testimonial/data.js
@@ -0,0 +1,55 @@
+export const stories = [
+    {
+      profileImg: "./testimonial/assets/profile-1.webp",
+      profileName: "Yuki",
+      title: [
+        "Captured magic!",
+        "what we envisioned!",
+        "Top-notch!"
+      ],
+      linkLabel: "Vist Yuki",
+      linkSrc: "https://www.instagram.com/yukiblr?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+      storyImg: "./testimonial/assets/story-1.webp",
+    },
+    {
+      profileImg: "./testimonial/assets/profile-2.webp",
+      profileName: "boba bhai",
+      title: ["Beyond our dreams!", "Unforgettable shots", "Simply stunning!"],
+      linkLabel: "Vist Boba",
+      linkSrc: "https://www.bobabhai.com",
+      storyImg: "../testimonial/assets/story-2.webp",
+    },
+    {
+      profileImg: "./testimonial/assets/profile-3.webp",
+      profileName: "Indudge",
+      title: ["Beautifully done!", "Art in every shot!", "Perfectly crafted!"],
+      linkLabel: "Check It Out",
+      linkSrc: "https://www.zomato.com/bangalore/indulge-cupcakes-kadubeesanahalli-bangalore",
+      storyImg: "./testimonial/assets/story-3.webp",
+    },
+    {
+      profileImg: "./testimonial/assets/profile-4.webp",
+      profileName: "Royal",
+      title: ["Flawless execution!", "Pure artistry!", "Impressive detail!"],
+      linkLabel: "More",
+      linkSrc: "#",
+      storyImg: "./testimonial/assets/story-4.webp",
+    },
+    {
+      profileImg: "./testimonial/assets/profile-5.webp",
+      profileName: "Something!",
+      title: ["Incredible vision!", "Truly captivating!", "Amazing perspective!"],
+      linkLabel: "Explore",
+      linkSrc: "#",
+      storyImg: "./testimonial/assets/story-5.webp",
+    },
+    {
+      profileImg: "./testimonial/assets/profile-6.webp",
+      profileName: "Yolo",
+      title: ["Every shot shines!", "Moments made timeless!", "and developers"],
+      linkLabel: "Visit Site",
+      linkSrc: "#",
+      storyImg: "./testimonial/assets/story-6.webp",
+    },
+  ];
+  
